1ad7f8884d500587938679c052b5b05da4532b6e
tusawe <joseluisgomezlopez91@gmail.com>
2019-10-07 10:15:50 +0200

    07/10_10:15

--- ProyectoF01/app/src/main/java/com/pmultimedia/proyectof01/Fragmento1.java
+++ ProyectoF01/app/src/main/java/com/pmultimedia/proyectof01/Fragmento1.java
@@ -1,4 +1,9 @@
 package com.pmultimedia.proyectof01;
 
-public class Fragmento1 {
+import androidx.fragment.app.Fragment;
+
+public class Fragmento1 extends Fragment {
+
+    public View onCreateView
+
 }
