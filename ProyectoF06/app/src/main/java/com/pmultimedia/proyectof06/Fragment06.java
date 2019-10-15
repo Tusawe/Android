5dc12d0d19fe2b175d9c8440901d85b2e1f10fd0
tusawe <joseluisgomezlopez91@gmail.com>
2019-10-15 10:05:04 +0200

    15/10_10:05

---
+++ ProyectoF06/app/src/main/java/com/pmultimedia/proyectof06/Fragment06.java
@@ -0,0 +1,14 @@
+package com.pmultimedia.proyectof06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fragment06 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment06);
+    }
+}
---
+++ ProyectoF06/app/src/main/java/com/pmultimedia/proyectof06/Fragmento1.java
@@ -0,0 +1,26 @@
+package com.pmultimedia.proyectof06;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebViewFragment;
+
+public class Fragmento1 extends WebViewFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = super.onCreateView(inflater,container,savedInstanceState);
+        WebView navegador = getWebView();
+        navegador.setWebViewClient(new WebViewClient());
+        navegador.loadUrl("https://www.omfgdogs.com/");
+        navegador.getSettings().setJavaScriptEnabled(true);
+        navegador.getSettings().setBuiltInZoomControls(true);
+
+        return view;
+
+    }
+}
