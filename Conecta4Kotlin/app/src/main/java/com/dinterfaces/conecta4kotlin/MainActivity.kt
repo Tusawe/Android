747dbcacfe525f25f3a68b1864cc9e516ec26a7d
Tusawe <joseluisgomezlopez91@gmail.com>
2019-10-23 17:58:24 +0200

    23/10_17:58

--- Conecta4Kotlin/app/src/main/java/com/dinterfaces/conecta4kotlin/MainActivity.kt
+++ Conecta4Kotlin/app/src/main/java/com/dinterfaces/conecta4kotlin/MainActivity.kt
@@ -4,15 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import java.util.*
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     var botones = Array<Array<Button?>>(7) { arrayOfNulls(6)}
+    lateinit var player1 : TextView
+    lateinit var player2 : TextView
+    lateinit var playerTurn : TextView
+    lateinit var players : String
     var player1points = 0
     var player2points = 0
     var player1turn = true
-    var turns = 1
+    var turns = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,13 +36,26 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         }
 
+        player1 = findViewById(R.id.textViewPlayer1) as TextView
+        player1.text = "Jugador 1-> ${player1points} puntos"
+
+        player2 = findViewById(R.id.textViewPlayer2) as TextView
+        player2.text = "Jugador 2-> ${player2points} puntos"
+
+        playerTurn = findViewById(R.id.textViewTurn) as TextView
+        if (player1turn) players = "jugador 1"
+        else players = "jugador 2"
+        playerTurn.text = "Turno del ${players}"
+
     }
 
     override fun onClick(v: View?) {
 
+        turns++
+
         val boton = v as Button
         var x = 0
-        var y = 0
+        // var y = 0
 
         // Averiguamos la posicion en el array.
         for (bx in 0 until 7) {
@@ -48,7 +65,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 if (boton.id == botones[bx][by]?.id) {
 
                     x = bx
-                    y = by
+                    // y = by
 
                 }
 
@@ -60,23 +77,203 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         for (fila in 5 downTo 0) {
 
-            if (botones[x][fila]?.text == "") {
+            if (setButton(x, fila)) break
+
+        }
+
+    }
+
+    private fun setButton(x: Int, fila: Int): Boolean {
+        if (botones[x][fila]?.text == "") {
+
+            if (player1turn) {
+                botones[x][fila]?.text = "X"
+            } else {
+                botones[x][fila]?.text = "O"
+            }
+
+            if (checkWinner()) {
 
                 if (player1turn) {
-                    botones[x][fila]?.text = "X"
-                }
-                else {
-                    botones[x][fila]?.text = "O"
+
+                    player1points++
+                    player1.text = "Jugador 1-> ${player1points} puntos"
+
+                } else {
+
+                    player2points++
+                    player2.text = "Jugador 2-> ${player2points} puntos"
+
                 }
 
-                turns++
-                player1turn = !player1turn
+                resetBoard()
 
-                break
+            } else {
+
+                if (turns == 42) {
+
+                    resetBoard()
+
+                }
+
+            }
+
+            player1turn = !player1turn
+
+            if (player1turn) players = "jugador 1"
+            else players = "jugador 2"
+
+            playerTurn.text = "Turno del ${players}"
+
+            return true
+
+        }
+        return false
+    }
+
+
+    private fun resetBoard() {
+
+        for (x in 0 until 7) {
+
+            for (y in 0 until 6) {
+
+                botones[x][y]?.text = ""
 
             }
 
         }
+
+        turns = 0
+        player1turn = false
+
+    }
+
+    private fun checkWinner() : Boolean {
+
+        // Comprobamos el 4 en línea en vertical.
+        for (i in 0 until 3) {
+
+            for (x in 0 until 7) {
+
+                if (botones[x][0+i]?.text == botones[x][1+i]?.text &&
+                    botones[x][0+i]?.text == botones[x][2+i]?.text &&
+                    botones[x][0+i]?.text == botones[x][3+i]?.text &&
+                    botones[x][0+i]?.text != "") return true
+
+            }
+
+        }
+
+        // Comprobamos el 4 en línea en horizontal.
+        for (i in 0 until 4) {
+
+            for (y in 0 until 6) {
+
+                if (botones[0+i][y]?.text == botones[1+i][y]?.text &&
+                    botones[0+i][y]?.text == botones[2+i][y]?.text &&
+                    botones[0+i][y]?.text == botones[3+i][y]?.text &&
+                    botones[0+i][y]?.text != "") return true
+
+            }
+
+        }
+
+        // Comprobamos el 4 en línea en diagonal de izquierdas y arriba a derechas y abajo.
+        if (botones[0][3]?.text == botones[1][2]?.text &&
+            botones[0][3]?.text == botones[2][1]?.text &&
+            botones[0][3]?.text == botones[3][0]?.text &&
+            botones[0][3]?.text != "") return true
+
+        for (i in 0 until 2) {
+
+            if (botones[0+i][4-i]?.text == botones[1+i][3-i]?.text &&
+                botones[0+i][4-i]?.text == botones[2+i][2-i]?.text &&
+                botones[0+i][4-i]?.text == botones[3+i][1-i]?.text &&
+                botones[0+i][4-i]?.text != "") return true
+
+        }
+
+        for (i in 0 until 3) {
+
+            if (botones[0+i][5-i]?.text == botones[1+i][4-i]?.text &&
+                botones[0+i][5-i]?.text == botones[2+i][3-i]?.text &&
+                botones[0+i][5-i]?.text == botones[3+i][2-i]?.text &&
+                botones[0+i][5-i]?.text != "") return true
+
+        }
+
+        for (i in 0 until 3) {
+
+            if (botones[1+i][5-i]?.text == botones[2+i][4-i]?.text &&
+                botones[1+i][5-i]?.text == botones[3+i][3-i]?.text &&
+                botones[1+i][5-i]?.text == botones[4+i][2-i]?.text &&
+                botones[1+i][5-i]?.text != "") return true
+
+        }
+
+        for (i in 0 until 2) {
+
+            if (botones[2+i][5-i]?.text == botones[3+i][4-i]?.text &&
+                botones[2+i][5-i]?.text == botones[4+i][3-i]?.text &&
+                botones[2+i][5-i]?.text == botones[5+i][2-i]?.text &&
+                botones[2+i][5-i]?.text != "") return true
+
+        }
+
+        if (botones[3][5]?.text == botones[4][4]?.text &&
+            botones[3][5]?.text == botones[5][3]?.text &&
+            botones[3][5]?.text == botones[6][2]?.text &&
+            botones[3][5]?.text != "") return true
+
+        // Comprobamos el 4 en línea en diagonal de izquierdas y abajo a derechas y arriba.
+        if (botones[0][2]?.text == botones[1][3]?.text &&
+            botones[0][2]?.text == botones[2][4]?.text &&
+            botones[0][2]?.text == botones[3][5]?.text &&
+            botones[0][2]?.text != "") return true
+
+        for (i in 0 until 2) {
+
+            if (botones[0+i][1+i]?.text == botones[1+i][2+i]?.text &&
+                botones[0+i][1+i]?.text == botones[2+i][3+i]?.text &&
+                botones[0+i][1+i]?.text == botones[3+i][4+i]?.text &&
+                botones[0+i][1+i]?.text != "") return true
+
+        }
+
+        for (i in 0 until 3) {
+
+            if (botones[0+i][0+i]?.text == botones[1+i][1+i]?.text &&
+                botones[0+i][0+i]?.text == botones[2+i][2+i]?.text &&
+                botones[0+i][0+i]?.text == botones[3+i][3+i]?.text &&
+                botones[0+i][0+i]?.text != "") return true
+
+        }
+
+        for (i in 0 until 3) {
+
+            if (botones[1+i][0+i]?.text == botones[2+i][1+i]?.text &&
+                botones[1+i][0+i]?.text == botones[3+i][2+i]?.text &&
+                botones[1+i][0+i]?.text == botones[4+i][3+i]?.text &&
+                botones[1+i][0+i]?.text != "") return true
+
+        }
+
+        for (i in 0 until 2) {
+
+            if (botones[2+i][0+i]?.text == botones[3+i][1+i]?.text &&
+                botones[2+i][0+i]?.text == botones[4+i][2+i]?.text &&
+                botones[2+i][0+i]?.text == botones[5+i][3+i]?.text &&
+                botones[2+i][0+i]?.text != "") return true
+
+        }
+
+        if (botones[3][0]?.text == botones[4][1]?.text &&
+            botones[3][0]?.text == botones[5][2]?.text &&
+            botones[3][0]?.text == botones[6][3]?.text &&
+            botones[3][0]?.text != "") return true
+
+        return false
 
     }
 
