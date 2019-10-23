7ca437297255ce36fc235a045db838c04a315097
tusawe <joseluisgomezlopez91@gmail.com>
2019-10-23 14:40:49 +0200

    23/10_14:40

--- Conecta4Kotlin/app/src/main/java/com/dinterfaces/conecta4kotlin/MainActivity.kt
+++ Conecta4Kotlin/app/src/main/java/com/dinterfaces/conecta4kotlin/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     var player1points = 0
     var player2points = 0
     var player1turn = true
-    var turns = 0
+    var turns = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +36,47 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View?) {
 
+        val boton = v as Button
+        var x = 0
+        var y = 0
 
+        // Averiguamos la posicion en el array.
+        for (bx in 0 until 7) {
+
+            for (by in 0 until 6) {
+
+                if (boton.id == botones[bx][by]?.id) {
+
+                    x = bx
+                    y = by
+
+                }
+
+            }
+
+        }
+
+        // Buscamos el ultimo valor
+
+        for (fila in 5 downTo 0) {
+
+            if (botones[x][fila]?.text == "") {
+
+                if (player1turn) {
+                    botones[x][fila]?.text = "X"
+                }
+                else {
+                    botones[x][fila]?.text = "O"
+                }
+
+                turns++
+                player1turn = !player1turn
+
+                break
+
+            }
+
+        }
 
     }
 
