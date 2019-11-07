65b46d48a28468b0901b4601348fa38c0a8626f6
tusawe <joseluisgomezlopez91@gmail.com>
2019-11-07 14:40:46 +0100

    07/11_14:40

--- BookStore/app/src/main/java/com/dinterfaces/bookstore/MainActivity.kt
+++ BookStore/app/src/main/java/com/dinterfaces/bookstore/MainActivity.kt
@@ -2,11 +2,23 @@ package com.dinterfaces.bookstore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 
 class MainActivity : AppCompatActivity() {
 
+    private var listaEditoriales =
+        arrayOf("Anaya", "Mac Graw Hill", "Oreilly", "Apress", "Manning", "Pretince Hall", "Rama")
+    lateinit var editorial : AutoCompleteTextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.formulario)
+
+        val editorialAdapter = ArrayAdapter(this, android.R.layout.select_dialog_item, listaEditoriales)
+        editorial = findViewById(R.id.editorial)
+        editorial.threshold = 2
+        editorial.setAdapter(editorialAdapter)
+
     }
 }
