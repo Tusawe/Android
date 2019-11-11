07c34ad06406e003270a5175d3a2edca371ae627
Tusawe <joseluisgomezlopez91@gmail.com>
2019-11-11 20:08:31 +0100

    11/11_20:08

---
+++ BookStore/app/src/main/java/com/dinterfaces/bookstore/Libro.kt
@@ -0,0 +1,5 @@
+package com.dinterfaces.bookstore
+
+class Libro (titulo : String, portada : String, isbn : String, autores : String,
+             encuadernacion : String, fPublicacion : String, sinopsis : String,
+             nPaginas : Int, precio : Float, formato : Boolean)
