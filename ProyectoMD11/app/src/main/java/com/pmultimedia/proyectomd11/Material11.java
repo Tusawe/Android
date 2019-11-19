ad8230ccc1a22824567778013aa2d41f59e8cae2
tusawe <joseluisgomezlopez91@gmail.com>
2019-11-19 10:14:58 +0100

    19/11_10:14

---
+++ ProyectoMD11/app/src/main/java/com/pmultimedia/proyectomd11/Material11.java
@@ -0,0 +1,22 @@
+package com.pmultimedia.proyectomd11;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Material11 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material11);
+    }
+}
